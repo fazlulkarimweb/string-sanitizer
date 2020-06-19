6b51ba6499c5fa79c5f2cbc5ab53d62119a59f07
T. Todua <7117978+ttodua@users.noreply.github.com>
2020-06-19 21:09:04 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 "use strict";
 
-exports.sanitize = function(str) {
-  return str.replace(/[^a-zA-Z0-9]/g, "");
+exports.sanitize = function(str, with) {
+  return str.replace(/[^a-zA-Z0-9]/g, with || "");
 };
 
-exports.sanitize.keepUnicode = function(str) {
-  return str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+exports.sanitize.keepUnicode = function(str, with) {
+  return str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, with || "");
 };
 
-exports.sanitize.keepSpace = function(str) {
-  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+exports.sanitize.keepSpace = function(str, with) {
+  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, with || "");
   return str2.replace(/ /g, " ");
 };
 
-exports.sanitize.addFullstop = function(str) {
-  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+exports.sanitize.addFullstop = function(str, with) {
+  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, with || "");
   return str2.replace(/ /g, ".");
 };
 
-exports.sanitize.addUnderscore = function(str) {
-  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+exports.sanitize.addUnderscore = function(str, with) {
+  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, with || "");
   return str2.replace(/ /g, "_");
 };
 
-exports.sanitize.addDash = function(str) {
-  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, "");
+exports.sanitize.addDash = function(str, with) {
+  var str2 = str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, with || "");
   return str2.replace(/ /g, "-");
 };
 
-exports.sanitize.removeNumber = function(str) {
-  return str.replace(/[^a-zA-Z]/g, "");
+exports.sanitize.removeNumber = function(str, with) {
+  return str.replace(/[^a-zA-Z]/g, with || "");
 };
 
-exports.sanitize.keepNumber = function(str) {
-  return str.replace(/[^a-zA-Z0-9]/g, "");
+exports.sanitize.keepNumber = function(str, with) {
+  return str.replace(/[^a-zA-Z0-9]/g, with || "");
 };
 
 // Add Fullstop, Underscore & Dash without sanitizing
 
-exports.addFullstop = function(str) {
-  return str.replace(/ /g, ".");
+exports.addFullstop = function(str, with) {
+  return str.replace(/ /g, with || ".");
 };
-exports.addUnderscore = function(str) {
-  return str.replace(/ /g, "_");
+exports.addUnderscore = function(str, with) {
+  return str.replace(/ /g, with || "_");
 };
 
-exports.addDash = function(str) {
-  return str.replace(/ /g, "-");
+exports.addDash = function(str, with) {
+  return str.replace(/ /g, with || "-");
 };
 
 // Remove Space without sanitizing
-exports.removeSpace = function(str) {
-  return str.replace(/\s+/g, "");
+exports.removeSpace = function(str, with) {
+  return str.replace(/\s+/g, with || "");
 };
