e6f073be3b84fb633841f6eb78b8564329438140
Md Fazlul Karim <fazlulkarimrocky@gmail.com>
2021-06-20 16:24:57 +0600

    Major Update Validation Added

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
-# String Sanitizer
+# String Sanitizer 
 
 An intuitive & tiny string sanitizer to remove any special characters or convert strings to create filename or url 🎉🎉
+
+# Version 2 with no breaking changes
+2.0.0 is launched with major updates. Pull requests are welcome. Everything is tested. Email, password and username validation is added.
 
 # Use Case
 
@@ -31,7 +34,7 @@ let someString = "@abcde$f$gh";
 string.sanitize(someString); // abcdefgh
 ```
 
-# Other Use Cases
+# Sanitization Use Cases
 
 ```js
 var string = require("string-sanitizer");
@@ -48,15 +51,83 @@ string.addFullstop("abcd efgh"); // abcd.efgh
 string.addUnderscore("@abcd efgh"); // @abcd_efgh
 string.addDash("@abcd efgh"); // @abcd-efgh
 string.removeSpace("@abcd efgh"); // @abcdefgh
+string.removeUnderscore("@ab__cd ef_gh_"); // @abcd efgh
 ```
 
-## Show Me the Code
+## Santization Use Case
 
 ![string-sanitizer](https://i.ibb.co/y44bXBb/Screenshot-275.png)
 
+## Validation (Added in version 2.00)
+Most of the time we have to validate email, password and username in our codebase. So string sanitizer starts offering validation. You pass your user generated email, username or password in this method. If it passed the checking, it will return the string as it is. If it doesn't pass the filtering, it will return false.
+
+ if you pass your email, username or password.
+
+### Email Validation
+
+```js
+var string = require("string-sanitizer");
+
+string.validate.isEmail("jhon@gmail.com") //  jhon@gmail.com
+string.validate.isEmail("jhongmail.com") // false
+string.validate.isEmail("jhon@gmailcom") // false
+string.validate.isEmail("jhon@@gmail.com") // false
+```
+
+### Username Validation
+Username must be free from any special characters. There will be no space and must be at least 2 characters long. Combination of numbers and letters is acceptable. Only numbers (i.e 123) are not acceptable. But only letters (i.e ea) wil be acceptable. 
+
+```js
+var string = require("string-sanitizer");
+
+string.validate.isUsername("fazlulka") // fazlulka
+string.validate.isUsername("Fazlulka") //  fazlulka (Automatically lowerstring method applied.)
+string.validate.isUsername("f") //  false (Minimum 2 letters)
+string.validate.isUsername("123") //  false (Only number is not acceptable)
+string.validate.isUsername("fazlulka@") //  false (Special Character not accpeted)
+string.validate.isUsername("fazlulka_") // false (Special Character not accepted)
+```
+
+Why minimum 2 letters not 3 letters?
+Because some of the username like (@ea) is still most popular.
+
+Why automatically lowerstring applied?
+Because, most of the end user still don't understand the meaning of username. Sometimes they use upper letter. We just sanitized it. Nothing more.
+
+### Password Validation
+
+#### To check a password between 6 to 15 characters which contain at least one numeric digit and a special character (Most popular)
+
+```js
+string.validate.isPassword6to15("password1@") //  password1@
+string.validate.isPassword6to15("password1") //  No special character
+```
+
+#### To check a password between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character (Most Secure)
+
+```js
+string.validate.isPassword8to15("password1Aa_"); //  password1Aa_
+string.validate.isPassword8to15("password1") // false ( Not matched)
+```
+
+#### To check a password between 7 to 20 characters which contain only characters, numeric digits, underscore and first character must be a letter. No special character accepted here. (Simple password)
+
+```js
+string.validate.isPassword7to20("password1@_") //  false (Because special character)
+string.validate.isPassword7to20("password1") //  password1
+```
+
+#### To check a password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter
+
+```js
+string.validate.isPassword6to20("password1Aa"); //  password1Aa
+string.validate.isPassword6to20("password1") // false ( Not matched)
+```
+
 ## Typescript compatitibility
 
-Thanks to @kewitz
+Thanks to @kewitz for typescript compatibility
+
 
 ## Contributing
 
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var string = require("./index");
+var string = require('./index');
 
 // string.sanitize("a.bc@d efg#h") =  abcdefgh
 // string.sanitize.keepSpace("a.bc@d efg#h") =  abcd efgh
@@ -15,67 +15,162 @@ var string = require("./index");
 
 console.log(
   `string.sanitize("a.bc@d efg#h") = `,
-  string.sanitize("a.bc@d efg#h")
+  string.sanitize('a.bc@d efg#h')
 ); // abcdefgh
 
 console.log(
   `string.sanitize.keepSpace("a.bc@d efg#h") = `,
-  string.sanitize.keepSpace("a.bc@d efg#h")
+  string.sanitize.keepSpace('a.bc@d efg#h')
 ); // abcd efgh
 
 console.log(
   `string.sanitize.keepUnicode("a.bc@d efg#hক") = `,
-  string.sanitize.keepSpace("a.bc@d efg#hক")
+  string.sanitize.keepSpace('a.bc@d efg#hক')
 ); // abcd efghক
 
 console.log(
   `string.sanitize.addFullstop("a.bc@d efg#h") = `,
-  string.sanitize.addFullstop("a.bc@d efg#h")
+  string.sanitize.addFullstop('a.bc@d efg#h')
 ); // abcd.efgh
 
 console.log(
   `string.sanitize.addUnderscore("a.bc@d efg#h") = `,
-  string.sanitize.addUnderscore("a.bc@d efg#h")
+  string.sanitize.addUnderscore('a.bc@d efg#h')
 ); // abcd_efgh
 
 console.log(
   `string.sanitize.addDash("a.bc@d efg#h") = `,
-  string.sanitize.addDash("a.bc@d efg#h")
+  string.sanitize.addDash('a.bc@d efg#h')
 ); // abcd-efgh
 
 console.log(
   `string.sanitize.removeNumber("@abcd efgh123") = `,
-  string.sanitize.removeNumber("@abcd efgh123")
+  string.sanitize.removeNumber('@abcd efgh123')
 ); // abcdefgh
 
 console.log(
   `string.sanitize.removeText("@abcd efgh123") = `,
-  string.sanitize.removeText("@abcd efgh123")
+  string.sanitize.removeText('@abcd efgh123')
 ); // 123
 
 console.log(
   `string.sanitize.keepNumber("@abcd efgh123") = `,
-  string.sanitize.keepNumber("@abcd efgh123")
+  string.sanitize.keepNumber('@abcd efgh123')
 ); // abcdefgh123
 
 console.log(
   `string.addFullstop("abcd efgh") = `,
-  string.addFullstop("abcd efgh")
+  string.addFullstop('abcd efgh')
 ); // abcd.efgh
 
 console.log(
   `string.addUnderscore("@abcd efgh") = `,
-  string.addUnderscore("@abcd efgh")
+  string.addUnderscore('@abcd efgh')
 ); // @abcd_efgh
 
-console.log(`string.addDash("@abcd efgh") = `, string.addDash("@abcd efgh")); // @abcd-efgh
+console.log(`string.addDash("@abcd efgh") = `, string.addDash('@abcd efgh')); // @abcd-efgh
 
 console.log(
   `string.removeSpace("@abcd efgh") = `,
-  string.removeSpace("@abcd efgh")
+  string.removeSpace('@abcd efgh')
 ); //  @abcdefgh
 
 console.log(
   `string.removeUnderscore("@ab__cd ef_gh_") = `,
-  string.removeUnderscore("@ab__cd ef_gh_")
+  string.removeUnderscore('@ab__cd ef_gh_')
 ); //  @abcd efgh
+
+console.log(
+  `string.validate.isEmail("fazlulkarimrocky@gmail.com") = `,
+  string.validate.isEmail('fazlulkarimrocky@gmail.com')
+); //  true
+
+console.log(
+  `string.validate.isEmail("fazlulkarimrockygmail.com") = `,
+  string.validate.isEmail('fazlulkarimrockygmail.com')
+); //  false
+
+console.log(
+  `string.validate.isEmail("fazlulkarimrocky@gmailcom") = `,
+  string.validate.isEmail('fazlulkarimrocky@gmailcom')
+); //  false
+
+console.log(
+  `string.validate.isEmail("fazlulkarimrocky@@gmail.com") = `,
+  string.validate.isEmail('fazlulkarimrocky@@gmail.com')
+); //  false
+
+console.log(
+  `string.validate.isUsername("fazlulka") = `,
+  string.validate.isUsername('fazlulka')
+); //  True
+
+console.log(
+  `string.validate.isUsername("f") = `,
+  string.validate.isUsername('f')
+); //  False
+
+console.log(
+  `string.validate.isUsername("123") = `,
+  string.validate.isUsername('123')
+); //  False
+
+console.log(
+  `string.validate.isUsername("Fazlulka") = `,
+  string.validate.isUsername('Fazlulka')
+); //  false
+
+console.log(
+  `string.validate.isUsername("fazlulka@") = `,
+  string.validate.isUsername('fazlulka@')
+); //  false
+
+console.log(
+  `string.validate.isUsername("fazlulka_") = `,
+  string.validate.isUsername('fazlulka_')
+); //  false
+
+//Password
+// 6 to 15
+console.log(
+  `string.validate.isPassword6to15("password1@_") = `,
+  string.validate.isPassword6to15('password1@_')
+); //  password1@
+
+console.log(
+  `string.validate.isPassword6to15("password1@") = `,
+  string.validate.isPassword6to15('password1@')
+); //  false
+
+// isPassword7to20
+console.log(
+  `string.validate.isPassword7to20("password1@_") = `,
+  string.validate.isPassword7to20('password1@_')
+); //  password1@
+
+console.log(
+  `string.validate.isPassword7to20("password1") = `,
+  string.validate.isPassword7to20('password1')
+); //  false
+
+// isPassword6to20
+console.log(
+  `string.validate.isPassword6to20("password1Aa") = `,
+  string.validate.isPassword6to20('password1Aa')
+); //  password1Aa_
+
+console.log(
+  `string.validate.isPassword6to20("password1") = `,
+  string.validate.isPassword6to20('password1')
+); //  false
+
+// isPassword8to20
+console.log(
+  `string.validate.isPassword8to15("password1Aa_") = `,
+  string.validate.isPassword8to15('password1Aa_')
+); //  password1Aa_
+
+console.log(
+  `string.validate.isPassword8to15("password1") = `,
+  string.validate.isPassword8to15('password1')
+); //  false
